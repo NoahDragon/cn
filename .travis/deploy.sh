31f965b177044e0623f593b487529b0fc2442176
Abner Chou <zhcjtht@hotmail.com>
2016-09-26 17:28:05 -0400

    Add travis support

---
+++ .travis/deploy.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# Set Git config
+git config --global user.name "Abner Chou"
+git config --global user.email contact@abnerchou.me
+# Clone the repository
+git clone --branch gh-pages git@github.com:NoahDragon/cn.git .deploy_git
+# Deploy to GitHub
+npm run deploy
