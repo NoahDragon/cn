88ab26df34bfbececeb4a09eae20bc6082e71b12
Abner Chou <zhcjtht@hotmail.com>
2016-12-07 21:48:27 -0500

    Change orders

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,6 @@
 
 #!/bin/bash
 
-# Purge CDN
-curl "https://api.keycdn.com/zones/purgeurl/$CDN_ZONE_ID.json" -u $CDN_API: -X DELETE -H "Content-Type: application/json" --data '{"urls":["'"$CDN_ZONE_URL"'/index.html","'"$CDN_ZONE_URL"'/tags/index.html", "'"$CDN_ZONE_URL"'/categories/index.html", "'"$CDN_ZONE_URL"'/archives/index.html", "'"$CDN_ZONE_URL"'/sitemap.xml", "'"$CDN_ZONE_URL"'/page-sitemap.xml", "'"$CDN_ZONE_URL"'/post-sitemap.xml", "'"$CDN_ZONE_URL"'/tag-sitemap.xml","'"$CDN_ZONE_URL"'/category-sitemap.xml"]}'
-
 # Set Git config
 git config --global user.name "Abner Chou"
 git config --global user.email contact@abnerchou.me
@@ -11,3 +8,6 @@ git config --global user.email contact@abnerchou.me
 git clone --branch gh-pages git@git.coding.net:AbnerChou/Abnerchou.git .deploy_git
 # Deploy to GitHub
 npm run deploy
+# Purge CDN
+curl "https://api.keycdn.com/zones/purgeurl/$CDN_ZONE_ID.json" -u $CDN_API: -X DELETE -H "Content-Type: application/json" --data '{"urls":["'"$CDN_ZONE_URL"'/index.html","'"$CDN_ZONE_URL"'/tags/index.html", "'"$CDN_ZONE_URL"'/categories/index.html", "'"$CDN_ZONE_URL"'/archives/index.html", "'"$CDN_ZONE_URL"'/sitemap.xml", "'"$CDN_ZONE_URL"'/page-sitemap.xml", "'"$CDN_ZONE_URL"'/post-sitemap.xml", "'"$CDN_ZONE_URL"'/tag-sitemap.xml","'"$CDN_ZONE_URL"'/category-sitemap.xml"]}'
+
