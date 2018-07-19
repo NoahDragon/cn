05305141c73a2d2d9eb989435aae89a2912ed879
Abner Chou <zhcjtht@hotmail.com>
2018-07-19 14:06:55 -0400

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,6 @@
 git config --global user.name "Abner Chou"
 git config --global user.email contact@abnerchou.me
 # Clone the repository
-git clone --branch gh-pages git@git.coding.net:AbnerChou/Abnerchou.git .deploy_git
+git clone --branch gh-pages git@github.com:NoahDragon/cn.git .deploy_git
 # Deploy to GitHub
 npm run deploy
